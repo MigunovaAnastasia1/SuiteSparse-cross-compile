812282bafe2113bad49bfa47336cf9006e61a8d5
Migunova Anastasia <anastasiamigunova76@gmail.com>
2024-11-30 07:03:41 +0300

    ci: some minor changesin cmake module for error correction

--- SuiteSparse_config/cmake_modules/SuiteSparse_cross_compile.cmake
+++ SuiteSparse_config/cmake_modules/SuiteSparse_cross_compile.cmake
@@ -2,13 +2,13 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 # which compilers to use for C and C++
-if ( $ENV{TARGET_ARCH} EQUAL "aarch64" OR $ENV{TARGET_ARCH} EQUAL "riscv64" )
+if ( $ENV{TARGET_ARCH} MATCHES "aarch64" OR $ENV{TARGET_ARCH} MATCHES "riscv64" )
     set(CMAKE_C_COMPILER   "/usr/bin/${TARGET_ARCH}-linux-gnu-gcc-10")
     set(CMAKE_CXX_COMPILER   "/usr/bin/${TARGET_ARCH}-linux-gnu-g++-9")
-elseif ( $ENV{TARGET_ARCH} EQUAL "ppc64le" )
+elseif ( $ENV{TARGET_ARCH} MATCHES "ppc64le" )
     set(CMAKE_C_COMPILER   "/usr/bin/powerpc64le-linux-gnu-gcc-10")
     set(CMAKE_CXX_COMPILER   "/usr/bin/powerpc64le-linux-gnu-g++-9")
-elseif ( $ENV{TARGET_ARCH} EQUAL "x86" )
+elseif ( $ENV{TARGET_ARCH} MATCHES "x86" )
     set(CMAKE_C_COMPILER   "/usr/bin/gcc")
     set(CMAKE_CXX_COMPILER   "/usr/bin/g++")
 else ( )
