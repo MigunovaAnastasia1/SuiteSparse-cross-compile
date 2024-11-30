98ed4985f8b57f2d41e0ecc210b66d8dd80a107d
Migunova Anastasia <anastasiamigunova76@gmail.com>
2024-11-30 04:15:33 +0300

    ci: added toolchain file for CMake

---
+++ SuiteSparse_config/cmake_modules/SuiteSparse_cross_compile.cmake
@@ -0,0 +1,25 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Linux)
+
+# which compilers to use for C and C++
+if ( TARGET_ARCH aarch64 || TARGET_ARCH riscv64 )
+    set(CMAKE_C_COMPILER   ${TARGET_ARCH}-linux-gnu-gcc-10)
+    set(CMAKE_CXX_COMPILER   ${TARGET_ARCH}-linux-gnu-g++-9)
+elseif ( TARGET_ARCH ppc64le )
+    set(CMAKE_C_COMPILER   powerpc64le-linux-gnu-gcc-10)
+    set(CMAKE_CXX_COMPILER   powerpc64le-linux-gnu-g++-9)
+else ( )
+    set(CMAKE_C_COMPILER   s390x-linux-gnu-gcc-10)
+    set(CMAKE_CXX_COMPILER   s390x-linux-gnu-g++-9)
+endif ( )
+
+# where is the target environment located
+set(CMAKE_FIND_ROOT_PATH CROSS_SYSROOT)
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
